acd7477c31c7207b645ee282bb76d27e103ba45c
udonwajnr <udonwajnr10@gmail.com>
2024-12-06 18:10:41 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-5xl text-orange-500 font-bold font-circular-web">Welcome to Awwwards</h1>
+      <h1 className="text-5xl text-violet-300 font-bold font-circular-web">Welcome to Awwwards</h1>
     </main>
   );
 }
